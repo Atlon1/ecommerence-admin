89a1c4ba251d46c969a1ffb696caedaed9084461
Atlon1 <maciej.szajstek@gmail.com>
2023-09-17 19:02:19 +0200

    update

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className='p-4'>
+            <Button>Click me</Button>
+        </div>
+    );
+};
+
+export default Page;
