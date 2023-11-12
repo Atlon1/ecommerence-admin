077a8edf19a0bc36ce5707acb56f59818a4a5b1c
Atlon1 <maciej.szajstek@gmail.com>
2023-11-12 17:30:43 +0100

    Dashborad

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,6 +1,22 @@
-const DashboardPage = () => {
+
+import prismadb from "@/lib/prismadb";
+
+type DashboradPageProps = {
+    params: {
+        storeId: string;
+    }
+}
+const DashboardPage = async  ({params} : DashboradPageProps) => {
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId
+        }
+    })
+
     return (
-        <div>DashboardPage</div>
+        <div>
+            Active store: {store?.name}
+        </div>
     )
 }
 export default DashboardPage
