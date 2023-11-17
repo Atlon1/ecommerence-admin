0b5b5d4a808590ae660697cbd31eb804defe892a
Atlon1 <maciej.szajstek@gmail.com>
2023-11-18 00:47:15 +0100

    settings prepere

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {auth} from "@clerk/nextjs";
+import {redirect} from "next/navigation";
+import prismatic from "@/lib/prismadb";
 
 interface SettingsPageProps {
     params: {
@@ -7,13 +9,30 @@ interface SettingsPageProps {
     }
 }
 
-const SettingsPage: React.FC<SettingsPageProps> = () => {
+const SettingsPage: React.FC<SettingsPageProps> =  async ({params}) => {
     const {userId} = auth();
+
+    if (!userId){
+        redirect('/sign-in')
+    }
+
+    const store = await prismatic.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    });
+
+    if(!store){
+        redirect('/')
+    }
 
 
     return (
-        <div>
-            Hello settings
+        <div className='flex-col'>
+            <div className='flex-1 space-y-4 p-8 pt-6'>
+                Hello settings page
+            </div>
         </div>
     );
 };
