0654587303f29e8c6069e9bd543e3beb4785bded
Atlon1 <maciej.szajstek@gmail.com>
2023-11-24 17:28:44 +0100

    delete btn shop added

--- components/modals/alert-modal.tsx
+++ components/modals/alert-modal.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, {useEffect, useState} from 'react';
 import {Modal} from '@/components/ui/modal';
+import {Button} from "@/components/ui/button";
 
 interface AlertModalProps {
     isOpen: boolean;
@@ -34,8 +35,13 @@ export const AlertModal: React.FC<AlertModalProps> = ({
             isOpen={isOpen}
             onClose={onClose}
         >
-            <div className='pt-6 space-x-2 flex items-center justify-end'>
-
+            <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+                <Button disabled={loading} variant='outline' onClick={onClose}>
+                    Cancel
+                </Button>
+                <Button disabled={loading} variant='destructive' onClick={onConfirm}>
+                    Continue
+                </Button>
             </div>
         </Modal>
     )
