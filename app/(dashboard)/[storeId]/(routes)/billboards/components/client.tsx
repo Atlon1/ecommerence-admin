4cacbc1ae58903cdabb3902f9c3857d3e57f9aa6
Atlon1 <maciej.szajstek@gmail.com>
2023-11-28 00:05:15 +0100

    billboard added form

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -16,7 +16,9 @@ export const BillboardClient = () => {
                 <Heading title='Billboards (0)'
                          description='Manage billboards for your store'
                 />
-                <Button>
+                <Button
+                onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+                >
                     <Plus className='mr-2 h-4 w-4'/>
                     Add New
                 </Button>
