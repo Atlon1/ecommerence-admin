60dc28f924b2e2e0e53a10dd94b24cf013113083
Atlon1 <maciej.szajstek@gmail.com>
2023-12-07 20:52:56 +0100

    Billboards table rdy

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -81,7 +81,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({initialData}) => {
             setLoading(true)
             await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`)
             router.refresh()
-            router.push('/')
+            router.push(`/${params.storeId}/billboards`)
             toast.success('Billboard deleted')
         }
         catch (error) {
@@ -165,7 +165,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({initialData}) => {
                     </Button>
                 </form>
             </Form>
-            <Separator/>
+
         </>
     )
 }
