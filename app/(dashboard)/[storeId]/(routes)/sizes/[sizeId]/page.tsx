73f430f2843e80146f16814ce6056ae0286141d9
Atlon1 <maciej.szajstek@gmail.com>
2023-12-12 22:17:36 +0100

    sizes property rdy

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
@@ -0,0 +1,21 @@
+import prismadb from "@/lib/prismadb";
+import {BillboardForm} from "./components/billboard-form";
+
+const SizePage = async ({params}: { params: { sizeId: string } }) => {
+
+    const size = await prismadb.size.findUnique({
+        where: {
+            id: params.sizeId
+        }
+    })
+
+    return (
+        <div className='flex-col'>
+           <div className='flex-1 space-y-4 p-8 pt-6'>
+               <BillboardForm initialData={size}/>
+           </div>
+        </div>
+    )
+}
+
+export default SizePage
--- app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -6,36 +6,36 @@ import {useRouter, useParams} from "next/navigation";
 import {Heading} from "@/components/ui/heading";
 import {Button} from "@/components/ui/button";
 import {Separator} from "@/components/ui/separator";
-import {BillboardColumn, columns} from "./columns";
+import {SizeColumn, columns} from "./columns";
 import {DataTable} from "@/components/ui/data-table";
 import {ApiList} from "@/components/ui/api-list";
 
-interface BillboardClientProps {
-    data: BillboardColumn[]
+interface SizesClientProps {
+    data: SizeColumn[]
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({ data}) => {
+export const SizesClient: React.FC<SizesClientProps> = ({ data}) => {
     const router = useRouter()
     const params = useParams()
 
     return (
         <>
             <div className='flex items-center justify-between'>
-                <Heading title={`Billboards (${data.length})`}
-                         description='Manage billboards for your store'
+                <Heading title={`Sizes (${data.length})`}
+                         description='Manage sizes for your store'
                 />
                 <Button
-                onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+                onClick={() => router.push(`/${params.storeId}/sizes/new`)}
                 >
                     <Plus className='mr-2 h-4 w-4'/>
                     Add New
                 </Button>
             </div>
             <Separator/>
-            <DataTable searchKey='label' columns={columns} data={data}/>
-            <Heading title='Api' description='Api calss for billboards'/>
+            <DataTable searchKey='name' columns={columns} data={data}/>
+            <Heading title='Api' description='Api calss for sizes'/>
             <Separator/>
-            <ApiList entityName='billboards' entityIdName='billboardId'/>
+            <ApiList entityName='sizes' entityIdName='sizeId'/>
         </>
     )
 }
