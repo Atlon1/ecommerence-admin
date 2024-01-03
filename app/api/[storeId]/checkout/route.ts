5b5c2692154340b0046d59a85029dd4986e3a91d
Atlon1 <maciej.szajstek@gmail.com>
2024-01-03 22:15:22 +0100

    update

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,27 +1,27 @@
 import Stripe from "stripe";
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
-import {stripe} from "@/lib/stripe";
+import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
 const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization"
-}
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
 export async function OPTIONS() {
-    return NextResponse.json({}, {headers: corsHeaders})
+    return NextResponse.json({}, { headers: corsHeaders });
 }
 
 export async function POST(
     req: Request,
-    {params}: { params: { storeId: string } }
+    { params }: { params: { storeId: string } }
 ) {
-    const {productIds} = await req.json();
+    const { productIds } = await req.json();
 
-    if (productIds || productIds.length === 0) {
-        return new NextResponse("Product id is required", {status: 400});
+    if (!productIds || productIds.length === 0) {
+        return new NextResponse("Product ids are required", { status: 400 });
     }
 
     const products = await prismadb.product.findMany({
@@ -30,24 +30,24 @@ export async function POST(
                 in: productIds
             }
         }
-    })
+    });
 
-    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
 
     products.forEach((product) => {
         line_items.push({
             quantity: 1,
             price_data: {
-                currency: "usd",
+                currency: 'USD',
                 product_data: {
-                    name: product.name
+                    name: product.name,
                 },
                 unit_amount: product.price.toNumber() * 100
             }
-        })
-    })
+        });
+    });
 
-    const oreder = await prismadb.order.create({
+    const order = await prismadb.order.create({
         data: {
             storeId: params.storeId,
             isPaid: false,
@@ -61,24 +61,23 @@ export async function POST(
                 }))
             }
         }
-    })
+    });
 
     const session = await stripe.checkout.sessions.create({
         line_items,
-        mode: "payment",
-        billing_address_collection: "required",
+        mode: 'payment',
+        billing_address_collection: 'required',
         phone_number_collection: {
-            enabled: true
+            enabled: true,
         },
         success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
         cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
         metadata: {
-            orderId: oreder.id
-        }
+            orderId: order.id
+        },
     });
 
-    return NextResponse.json({url: session.url}, {
+    return NextResponse.json({ url: session.url }, {
         headers: corsHeaders
-    })
-
-}
+    });
+};
