a50490ca6e1dbe8e6fa0ebacd7954050a182d0b1
Atlon1 <maciej.szajstek@gmail.com>
2024-01-04 23:38:13 +0100

    add dashborad component

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,21 +1,28 @@
 
 import prismadb from "@/lib/prismadb";
 
+import {Heading} from "@/components/ui/heading";
+import {Separator} from "@/components/ui/separator";
+
+
 interface DashboardPageProps {
     params: {
         storeId: string;
     }
 }
 const DashboardPage: React.FC<DashboardPageProps> = async  ({params}) => {
-    const store = await prismadb.store.findFirst({
-        where: {
-            id: params.storeId
-        }
-    })
+
 
     return (
-        <div>
-            Active store: {store?.name}
+        <div className='flex-col'>
+            <div className='flex-1 space-y-4 p-8 pt-6'>
+                <Heading title='Dashboard' description='Overview of your store'/>
+                <Separator/>
+                <div className='gird gap-4 grid-cols-3'>
+                    
+
+                </div>
+            </div>
         </div>
     )
 }
